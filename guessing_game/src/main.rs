2f700411639a50e5704de4a0aff49ac908380443
lucas <morettilucash@gmail.com>
2022-11-13 02:22:52 -0300

    loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     // println! is a macro that prints a string to the screen:
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess: String = String::new();
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}")
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 
+    // println!("You guessed: {}",guess)
 }
